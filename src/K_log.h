d120891d6e8432ac8b2be4a70606d74bdfac8cf7
nonk123 <me@nonk.dev>
2025-08-31 10:23:27 +0300

    Add Linux builds
    The CMakeage is extremely fucked up due to fmod.

--- src/K_log.h
+++ src/K_log.h
@@ -3,10 +3,10 @@
 #include <SDL3/SDL_log.h>
 #include <stdlib.h>
 
-#define INFO(...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, ##__VA_ARGS__)
+#define INFO(...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 
 #define FATAL(...)                                                                                                     \
     do {                                                                                                               \
-        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, ##__VA_ARGS__);                                                  \
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);                                                    \
         exit(EXIT_FAILURE);                                                                                            \
     } while (0)
