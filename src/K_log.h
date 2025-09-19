3fe3558c48a5c16871274b35c012abdc789107f9
nonk123 <me@nonk.dev>
2025-09-19 20:26:59 +0300

    Tidy up logging macros

--- src/K_log.h
+++ src/K_log.h
@@ -2,20 +2,13 @@
 
 #include <SDL3/SDL_log.h>
 
-#define INFO SDL_Log
+#define LOG_WITH(fn, ...) fn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 
-#define WARN(...)                                                                                                      \
-	do {                                                                                                           \
-		SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);                                                \
-	} while (0)
-
-#define WTF(...)                                                                                                       \
-	do {                                                                                                           \
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);                                               \
-	} while (0)
-
+#define INFO(...) LOG_WITH(SDL_LogInfo, __VA_ARGS__)
+#define WARN(...) LOG_WITH(SDL_LogWarn, __VA_ARGS__)
+#define WTF(...) LOG_WITH(SDL_LogError, __VA_ARGS__)
 #define FATAL(...)                                                                                                     \
 	do {                                                                                                           \
-		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);                                            \
+		LOG_WITH(SDL_LogCritical, __VA_ARGS__);                                                                \
 		exit(1);                                                                                               \
 	} while (0)
