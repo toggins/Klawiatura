a902d3507b67d075aa15f47255d5502ecf7f862d
nonk123 <me@nonk.dev>
2025-09-20 12:41:01 +0300

    Silence a warning when building under WSL

--- src/K_log.h
+++ src/K_log.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <SDL3/SDL_log.h>
+#include <stdlib.h> // IWYU pragma: keep
+// ^ required for `exit(1)` below
 
 #define LOG_WITH(fn, ...) fn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 
