524b17b045012126ad2b3f92dda08f6723e5fc53
nonk123 <me@nonk.dev>
2025-09-24 07:47:04 +0300

    Pick menu nits

--- src/K_menu.c
+++ src/K_menu.c
@@ -15,6 +15,7 @@ static void noop() {
 	static void fname() {                                                                                          \
 		set_menu(men);                                                                                         \
 	}
+
 // Main Menu
 SHORTCUT(goto_singleplayer, MEN_SINGLEPLAYER);
 SHORTCUT(goto_multiplayer, MEN_MULTIPLAYER);
@@ -62,6 +63,7 @@ static Menu MENUS[MEN_SIZE] = {
 	[MEN_OPTIONS] = {"Options"},
 	[MEN_CONTROLS] = {"Change Controls"},
 };
+
 static Option OPTIONS[MEN_SIZE][MAX_OPTIONS] = {
 	[MEN_MAIN] = {
 		{"Singleplayer", .callback = goto_singleplayer},
@@ -70,7 +72,7 @@ static Option OPTIONS[MEN_SIZE][MAX_OPTIONS] = {
 		{"Exit", .callback = instaquit},
 	},
 	[MEN_SINGLEPLAYER] = {
-		{"Level: FMT"},
+		{"Level: %s"},
 		{},
 		{"Start!"},
 	},
@@ -82,46 +84,46 @@ static Option OPTIONS[MEN_SIZE][MAX_OPTIONS] = {
 	[MEN_OPTIONS] = {
 		{"Change Controls", .callback = goto_controls},
 		{},
-		{"Name: FMT"},
-		{"Skin: FMT"},
+		{"Name: %s"},
+		{"Skin: %s"},
 		{},
-		{"Scale: FMT"},
-		{"Fullscreen: FMT"},
-		{"Vsync: FMT"},
+		{"Scale: %d"},
+		{"Fullscreen: %s"},
+		{"Vsync: %s"},
 		{},
-		{"Master Volume: FMT"},
-		{"Sound Volume: FMT"},
-		{"Music Volume: FMT"},
+		{"Master Volume: %d"},
+		{"Sound Volume: %d"},
+		{"Music Volume: %d"},
 	},
 	[MEN_CONTROLS] = {
-		{"Up: FMT"},
-		{"Left: FMT"},
-		{"Down: FMT"},
-		{"Right: FMT"},
+		{"Up: %s"},
+		{"Left: %s"},
+		{"Down: %s"},
+		{"Right: %s"},
 		{},
-		{"Jump: FMT"},
-		{"Run: FMT"},
-		{"Fire: FMT"},
+		{"Jump: %s"},
+		{"Run: %s"},
+		{"Fire: %s"},
 	},
 	[MEN_HOST_LOBBY] = {
 		// FIXME: Add explicit lobby hosting to NutPunch. (if the lobby ID already exists, add a "(1)" to it or something)
-		{"Lobby ID: FMT"},
+		{"Lobby ID: %s"},
 
 		// FIXME: Potential features for NutPunch.
-		{"Visibility: FMT", .disabled = true},
-		{"Password: FMT", .disabled = true},
+		{"Visibility: %s", .disabled = true},
+		{"Password: %s", .disabled = true},
 
 		{},
 
 		// NOTE: You can have peers beyond MAX_PLAYERS, they'll just become spectators.
-		{"Players: FMT"},
+		{"Players: %d"},
 
 		{},
 		{"Host!"},
 	},
 	[MEN_JOIN_LOBBY] = {
 		// FIXME: Add explicit lobby joining to NutPunch.
-		{"Lobby ID: FMT"},
+		{"Lobby ID: %s"},
 		{},
 		{"Join!"},
 	},
@@ -133,17 +135,12 @@ static Option OPTIONS[MEN_SIZE][MAX_OPTIONS] = {
 		{"Joining lobby \"FMT\"", .disabled = true},
 	},
 	[MEN_LOBBY] = {
-		{"Lobby: FMT", .disabled = true},
-		{"Level: FMT"},
+		{"Lobby: %s", .disabled = true},
+		{"Level: %s"},
 		{},
 		{"Start!"},
 	},
 };
-
-static void start_menu_fr() {
-	set_menu(MEN_MAIN);
-	play_generic_track("title", true);
-}
 
 void start_menu(bool skip_intro) {
 	from_scratch();
@@ -171,17 +168,14 @@ void start_menu(bool skip_intro) {
 	load_track("sf_map");
 	load_track("human_like_predator");
 
-	if (skip_intro)
-		start_menu_fr();
-	else
-		set_menu(MEN_INTRO);
+	set_menu(skip_intro ? MEN_MAIN : MEN_INTRO);
 }
 
 void update_menu() {
 	for (new_frame(); got_ticks(); next_tick()) {
 		if (menu == MEN_INTRO) {
 			if (totalticks() >= 150 || kb_pressed(KB_UI_ENTER))
-				start_menu_fr();
+				set_menu(MEN_MAIN);
 			continue;
 		}
 
@@ -191,7 +185,6 @@ void update_menu() {
 
 		size_t old_option = MENUS[menu].option;
 		size_t new_option = old_option;
-		bool found_option = false;
 
 		for (size_t i = 0; i < MAX_OPTIONS; i++) {
 			if (change < 0 && new_option <= 0)
@@ -202,13 +195,13 @@ void update_menu() {
 				new_option += change;
 
 			Option* option = &OPTIONS[menu][new_option];
-			if (option->name != NULL && !option->disabled) {
-				found_option = true;
-				break;
-			}
+			if (option->name != NULL && !option->disabled)
+				goto found_option;
 		}
+		goto try_select;
 
-		if (found_option && old_option != new_option) {
+	found_option:
+		if (old_option != new_option) {
 			MENUS[menu].option = new_option;
 			play_generic_sound("switch");
 		}
@@ -222,42 +215,28 @@ void update_menu() {
 			}
 		}
 
-		if (kb_pressed(KB_PAUSE))
-			switch (menu) {
-				// FIXME: Replace this non-default junk with actual lobby stuff
-				case MEN_JOINING_LOBBY: // Should abort connection and return to previous menu
-				case MEN_LOBBY: // Should disconnect and return to the menu before MEN_JOINING_LOBBY
-					noop();
-					break;
-
-				default: {
-					if (set_menu(MENUS[menu].from))
-						play_generic_sound("select");
-					break;
-				}
-			}
+		if (!kb_pressed(KB_PAUSE))
+			continue; // de-nesting
+		// TODO: stuff this into a cleanup function or something?
+		switch (menu) {
+			// FIXME: Replace this non-default junk with actual lobby stuff
+			case MEN_JOINING_LOBBY: // Should abort connection and return to previous menu
+			case MEN_LOBBY:         // Should disconnect and return to the menu before MEN_JOINING_LOBBY
+				noop();
+				break;
+			default:
+				if (set_menu(MENUS[menu].from))
+					play_generic_sound("select");
+				break;
+		}
 	}
 }
 
 void draw_menu() {
 	start_drawing();
 
-	if (menu == MEN_INTRO) {
-		clear_color(0, 0, 0, 1);
-
-		float a = 1.f;
-		const float ticks = totalticks();
-		if (ticks < 25)
-			a = ticks / 25.f;
-		if (ticks > 125)
-			a = 1.f - ((ticks - 100.f) / 25.f);
-
-		set_batch_alpha_test(0);
-		batch_sprite(
-			"ui/disclaimer", XYZ(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT, 0), NO_FLIP, 0, ALPHA(a * 255));
-		set_batch_alpha_test(0.5f);
-		goto jobwelldone;
-	}
+	if (menu == MEN_INTRO)
+		goto draw_intro;
 
 	batch_sprite("ui/background", XYZ(0, 0, 0), NO_FLIP, 0, WHITE);
 
@@ -289,17 +268,35 @@ void draw_menu() {
 			ALPHA(128));
 	}
 
+	goto jobwelldone;
+
+draw_intro:
+	clear_color(0, 0, 0, 1);
+
+	float a = 1.f;
+	const float ticks = totalticks();
+	if (ticks < 25)
+		a = ticks / 25.f;
+	if (ticks > 125)
+		a = 1.f - ((ticks - 100.f) / 25.f);
+
+	set_batch_alpha_test(0);
+	batch_sprite("ui/disclaimer", XYZ(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT, 0), NO_FLIP, 0, ALPHA(a * 255));
+	set_batch_alpha_test(0.5f);
+
 jobwelldone:
 	stop_drawing();
 }
 
-bool set_menu(MenuType men) {
-	if (menu == men || men <= MEN_NULL || men >= MEN_SIZE)
+bool set_menu(MenuType next_menu) {
+	if (menu == next_menu || next_menu <= MEN_NULL || next_menu >= MEN_SIZE)
 		return false;
 
-	if (MENUS[menu].from != men)
-		MENUS[men].from = MENUS[men].noreturn ? MEN_NULL : menu;
-	menu = men;
+	if (MENUS[menu].from != next_menu)
+		MENUS[next_menu].from = MENUS[next_menu].noreturn ? MEN_NULL : menu;
+	if ((menu == MEN_NULL || menu == MEN_INTRO) && next_menu == MEN_MAIN)
+		play_generic_track("title", true);
+	menu = next_menu;
 
 	// Go to nearest valid option
 	size_t new_option = MENUS[menu].option;
