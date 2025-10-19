0f56e7d0cbfd1fbcfce06336b3fc6d7643944205
toggins <localamnesiac+troll@gmail.com>
2025-10-19 23:41:14 +0300

    Bump GekkoNet (risk of segfault)

--- src/K_game.c
+++ src/K_game.c
@@ -122,7 +122,7 @@ void nuke_game() {
 	nuke_audio_state();
 	nuke_video_state();
 
-	gekko_destroy(game_session);
+	gekko_destroy(&game_session);
 	game_session = NULL;
 	destroy_surface(game_surface);
 	game_surface = NULL;
