03dc4f1886dfd03df81ca46dedafc6870fc87a4b
toggins <localamnesiac+troll@gmail.com>
2025-10-23 05:51:41 +0300

    Boundary triggers

---
+++ src/actors/K_bounds.c
@@ -0,0 +1,44 @@
+#include "K_game.h"
+
+enum {
+	VAL_BOUNDS_X1 = VAL_CUSTOM,
+	VAL_BOUNDS_Y1,
+	VAL_BOUNDS_X2,
+	VAL_BOUNDS_Y2,
+};
+
+static void load() {
+	load_sound("warp");
+}
+
+static void create(GameActor* actor) {
+	actor->box.end.x = actor->box.end.y = FfInt(32L);
+}
+
+static void collide(GameActor* actor, GameActor* from) {
+	if (from->type != ACT_PLAYER)
+		return;
+
+	GamePlayer* player = get_owner(from);
+	if (player == NULL)
+		return;
+	if (VAL(actor, VAL_BOUNDS_X1) == VAL(actor, VAL_BOUNDS_X2)
+		|| VAL(actor, VAL_BOUNDS_Y1) == VAL(actor, VAL_BOUNDS_Y2))
+		return;
+	if (player->bounds.start.x != VAL(actor, VAL_BOUNDS_X1) || player->bounds.start.y != VAL(actor, VAL_BOUNDS_Y1)
+		|| player->bounds.end.x != VAL(actor, VAL_BOUNDS_X2)
+		|| player->bounds.end.y != VAL(actor, VAL_BOUNDS_Y2))
+	{
+		player->bounds.start.x = VAL(actor, VAL_BOUNDS_X1), player->bounds.start.y = VAL(actor, VAL_BOUNDS_Y1),
+		player->bounds.end.x = VAL(actor, VAL_BOUNDS_X2), player->bounds.end.y = VAL(actor, VAL_BOUNDS_Y2);
+
+		// !!! CLIENT-SIDE !!!
+		if (viewplayer() == player->id) {
+			// lerp_camera(500);
+			play_state_sound("warp");
+		}
+		// !!! CLIENT-SIDE !!!
+	}
+}
+
+const GameActorTable TAB_BOUNDS = {.load = load, .create = create, .collide = collide};
