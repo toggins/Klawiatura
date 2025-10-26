093a604076c2303b23518c8ee42b840a68885f3f
nonk123 <me@nonk.dev>
2025-10-26 09:02:28 +0300

    Bump NutPunch

--- local_3p.bat
+++ local_3p.bat
@@ -1,3 +1,3 @@
-start cmd /c "build\Klawiatura" -ip 127.0.0.1 ^& pause
-start cmd /c "build\Klawiatura" -ip 127.0.0.1 ^& pause
-start cmd /c "build\Klawiatura" -ip 127.0.0.1 ^& pause
+start cmd /c "build\Klawiatura" -ip localhost ^& pause
+start cmd /c "build\Klawiatura" -ip localhost ^& pause
+start cmd /c "build\Klawiatura" -ip localhost ^& pause
