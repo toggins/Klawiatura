688942f72452a5ae969ad18bfe438a7f91d36b8d
nonk123 <me@nonk.dev>
2026-02-21 22:49:14 +0300

    address the comment for the previous commit

--- src/actors/K_benchmark.c
+++ src/actors/K_benchmark.c
@@ -1,10 +1,9 @@
 #include "K_game.h"
-#include "K_tick.h"
 
 #define DUMMY_ACTOR ACT_KOOPA
 
 enum {
-	VAL_DEATH_TIMER = MAX_VALUES - 1,
+	VAL_DEATH_TIMER = MAX_VALUES - 1L,
 };
 
 static void load() {
@@ -13,10 +12,10 @@ static void load() {
 
 static void tick(GameActor* self) {
 	for (GameActor* other = get_actor(game_state.live_actors); other != NULL; other = get_actor(other->previous))
-		if (other->type == DUMMY_ACTOR && VAL(other, DEATH_TIMER)++ >= 12 * TICKRATE)
+		if (other->type == DUMMY_ACTOR && VAL(other, DEATH_TIMER)++ >= 600L)
 			FLAG_ON(other, FLG_DESTROY);
 	GameActor* dummy = create_actor(DUMMY_ACTOR, self->pos);
-	VAL(dummy, DEATH_TIMER) = 0; // redundant but sure...
+	VAL(dummy, DEATH_TIMER) = 0L; // redundant but sure...
 }
 
 const GameActorTable TAB_BENCHMARK = {.load = load, .tick = tick};
