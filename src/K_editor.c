5d4b7763a8e4c86740a4f55dc498c9b97cb9c8f3
nonk123 <me@nonk.dev>
2026-03-02 13:50:37 +0300

    `Fixed` nitpique

--- src/K_editor.c
+++ src/K_editor.c
@@ -20,8 +20,9 @@ void editor_baton_pass(GamePlayer* player, GameActor* self) {
 	float mx = 0.f, my = 0.f;
 	get_cursor_pos(&mx, &my);
 
-	const Fixed dx = ANY_INPUT(player, GI_RIGHT) - ANY_INPUT(player, GI_LEFT),
-		    dy = ANY_INPUT(player, GI_UP) - ANY_INPUT(player, GI_DOWN), angle = FxAtan2(-dy, dx);
+	const Fixed dx = FxFrom(ANY_INPUT(player, GI_RIGHT) - ANY_INPUT(player, GI_LEFT)),
+		    dy = FxFrom(ANY_INPUT(player, GI_DOWN) - ANY_INPUT(player, GI_UP));
+	const Fixed angle = FxAtan2(dy, dx);
 
 	Fixed speed = ANY_INPUT(player, GI_RUN) ? FxFrom(20L) : FxFrom(8L);
 	if (!dx && !dy)
