3287bb52f9c3d3aafcd3bd8665b4c7b28dcee175
toggins <localamnesiac+troll@gmail.com>
2026-08-19 02:25:54 +0300

    Editor: Arrow keys to nudge selected marker

--- src/screens/K_editor.c
+++ src/screens/K_editor.c
@@ -163,7 +163,7 @@ static void move_cursor(const Sint32 pos[2], Bool snap, Bool highlight) {
     }
 
     if (highlight && !ecursor->has_scalable) {
-        Sint32 ox = ecursor->pos[0], oy = ecursor->pos[1];
+        const Sint32 ox = ecursor->pos[0], oy = ecursor->pos[1];
         if (pos[0] != ox || pos[1] != oy) {
             ecursor->has_highlighted = FALSE;
 
@@ -1306,6 +1306,7 @@ static void draw_ui() {
     ImGui_NewFrame();
 
     EditorCamera* ecamera = &editor->camera;
+    EditorCursor* ecursor = &editor->cursor;
 
     const ImGuiIO* io = ImGui_GetIO();
     if (!io->WantCaptureKeyboard) {
@@ -1327,9 +1328,19 @@ static void draw_ui() {
             if (ImGui_IsKeyPressed(ImGuiKey_E))
                 editor->def_key = cdef->next;
         }
+
+        if (ecursor->has_selected) {
+            if (ImGui_IsKeyPressedEx(ImGuiKey_UpArrow, TRUE))
+                --editor->level.markers[ecursor->selected].pos[1];
+            if (ImGui_IsKeyPressedEx(ImGuiKey_LeftArrow, TRUE))
+                --editor->level.markers[ecursor->selected].pos[0];
+            if (ImGui_IsKeyPressedEx(ImGuiKey_DownArrow, TRUE))
+                ++editor->level.markers[ecursor->selected].pos[1];
+            if (ImGui_IsKeyPressedEx(ImGuiKey_RightArrow, TRUE))
+                ++editor->level.markers[ecursor->selected].pos[0];
+        }
     }
 
-    EditorCursor* ecursor = &editor->cursor;
     if (!io->WantCaptureMouse) {
         ImVec2 mpos = ImGui_GetMousePos();
 
