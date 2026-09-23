694f6b8ae048dcd5197914750d7a44804dae92fd
toggins <localamnesiac+troll@gmail.com>
2026-09-23 22:39:14 +0300

    Volcano

--- src/actors/K_podoboo.c
+++ src/actors/K_podoboo.c
@@ -1,3 +1,4 @@
+#include "K_audio.h"
 #include "K_string.h"
 #include "K_video.h"
 
@@ -39,7 +40,7 @@ static void tick(GameActor* actor) {
     move_actor(actor, Vadd(actor->pos, actor->vel));
     actor->vel.y += 13107;
 
-    if (actor->pos.y > (levelinfo()->size.y + Int2Fx(32)))
+    if (below_nearest_bounds(actor->pos, Int2Fx(32)))
         FLAG_ON(actor, FLG_DESTROY);
 
     if (actor->vel.y > Fx0)
@@ -50,11 +51,16 @@ static void tick(GameActor* actor) {
     VAL_TICK(actor, PODOBOO_OVERLAP);
     collide_actor(actor);
     if (!ANY_FLAG(actor, FLG_VISIBLE)
-        || (actor->pos.y > VAL(actor, PODOBOO_Y) && actor->vel.y >= Fmul(VAL(actor, PODOBOO_JUMP), 43691)))
+        || (!ANY_FLAG(actor, FLG_PODOBOO_VOLCANO) && actor->pos.y > VAL(actor, PODOBOO_Y)
+            && actor->vel.y >= Fmul(VAL(actor, PODOBOO_JUMP), 43691)))
     {
         create_actor(ACT_LAVA_SPLASH, Vadd(actor->pos, (FVec2){Fx0, Int2Fx(15)}));
-        move_actor(actor, (FVec2){actor->pos.x, VAL(actor, PODOBOO_Y)});
-        FLAG_OFF(actor, FLG_VISIBLE);
+        if (ANY_FLAG(actor, FLG_PODOBOO_VOLCANO)) {
+            FLAG_ON(actor, FLG_DESTROY);
+        } else {
+            move_actor(actor, (FVec2){actor->pos.x, VAL(actor, PODOBOO_Y)});
+            FLAG_OFF(actor, FLG_VISIBLE);
+        }
     }
 }
 
@@ -86,4 +92,52 @@ const ActorTable TAB_PODOBOO = {
     .tick = tick,
     .draw = draw,
     .collide = collide,
+};
+
+/* ===============
+   PODOBOO VOLCANO
+   =============== */
+
+static void load_volcano() {
+    load_sound("fire", AKL_NEVER);
+    load_actor(ACT_PODOBOO);
+}
+
+static void create_volcano(GameActor* actor) {
+    VAL(actor, VOLCANO_TIME) = 300;
+    VAL(actor, VOLCANO_FIRE_TIME) = 15;
+    VAL(actor, VOLCANO_FIRE_AMOUNT) = 8;
+}
+
+static void tick_volcano(GameActor* actor) {
+    if (--VAL(actor, VOLCANO_STATE) <= 0) {
+        if (VAL(actor, VOLCANO_FIRE) <= 0)
+            VAL(actor, VOLCANO_FIRE) = VAL(actor, VOLCANO_FIRE_AMOUNT);
+
+        VAL(actor, VOLCANO_STATE) += VAL(actor, VOLCANO_TIME);
+    }
+
+    if (--VAL(actor, VOLCANO_FIRE_STATE) <= 0) {
+        if (VAL(actor, VOLCANO_FIRE) > 0 && in_any_view(actor->pos, Int2Fx(-256), VEF_ALL)) {
+            GameActor* podoboo = create_actor(ACT_PODOBOO, actor->pos);
+            if (podoboo != NULL) {
+                podoboo->vel.x = Int2Fx(rng(5));
+                podoboo->vel.x -= Int2Fx(rng(5));
+                podoboo->vel.y = Int2Fx(-11);
+                FLAG_ON(podoboo, FLG_VISIBLE | FLG_PODOBOO_VOLCANO);
+            }
+
+            --VAL(actor, VOLCANO_FIRE);
+
+            play_state_sound("fire", PLAY_POS, A_ACTOR(actor));
+        }
+
+        VAL(actor, VOLCANO_FIRE_STATE) += VAL(actor, VOLCANO_FIRE_TIME);
+    }
+}
+
+const ActorTable TAB_VOLCANO = {
+    .load = load_volcano,
+    .create = create_volcano,
+    .tick = tick_volcano,
 };
